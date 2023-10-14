2feaba82109ffa48cdb0178767aa906aaf1c0275
Douglas Martin <mdgeek1@gmail.com>
2023-10-14 10:14:41 -0400

    POM refactoring.

--- staging/gulpfile.js
+++ staging/gulpfile.js
@@ -1,6 +1,6 @@
 const {src, dest, series} = require('gulp');
 
-const srcDir = '${src.rootdir}/node_modules/jquery.scrollto/';
+const srcDir = '${webjar.staging}/node_modules/jquery.scrollto/';
 const destDir = '${webjar.target}/';
 
 function task1() {
